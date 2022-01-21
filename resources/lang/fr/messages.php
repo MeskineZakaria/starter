a7cebbd7309517b74245b7118982efc4adceb8bf
zakaria <zakaria.meskine16@gmail.com>
2022-01-21 17:13:15 +0100

    =first commit

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Réinitialiser',
+    'sent' => 'Envoyer',
+    'throttled' => "Attendez s'il vous plait avant de retenter.",
+    'token' => "Ce Token n'est pas valide",
+    'user' => "Nous ne pouvons pas trouver cet utilisateur",
+];
---
+++ resources/views/layouts/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
+
+<p>ca marche</p>
+@stop
